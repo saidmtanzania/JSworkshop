ab07625f8acf11232cef87f13f0e150f5f6e34c9
saidmtanzania <saidmtanzania@gmail.com>
2022-02-02 01:50:10 +0300

    initial changes

--- js/main.js
+++ js/main.js
@@ -16,12 +16,54 @@ JavaScript can "display" data in different ways:
     Writing into the browser console, using console.log()
  
 */
-    document.getElementById("dev").innerHTML = 5+1;
-    //Using document.write() after an HTML document is loaded, will delete all existing HTML:
-    document.write("take a bow");
-    //Using window.alert() You can use an alert box to display data:
-    window.alert("Hello JavaScript");
-    //window.print() to print the whole page
-    window.print();
-    //console.log()
-    console.log("Hello JavaScript")
+    // document.getElementById("dev").innerHTML = 5+1;
+    // //Using document.write() after an HTML document is loaded, will delete all existing HTML:
+    // document.write("take a bow");
+    // //Using window.alert() You can use an alert box to display data:
+    // window.alert("Hello JavaScript");
+    // //window.print() to print the whole page
+    // window.print();
+    // //console.log()
+    // console.log("Hello JavaScript")
+    //constant Array
+    const cars = ["Saad","volvo","BMW"];
+    cars[0] = "Toyota";
+    cars.push("Audi");
+    document.getElementById("dev").innerHTML = cars;
+
+    const Cars = {
+        type:"BMW", model:"X6", color:"white"
+    }
+    Cars.color = "blue";
+    Cars.owner = "saidmtanzania";
+    document.getElementById("devis").innerHTML = Cars.owner;
+    
+    let text = "what a very";
+    text+=" a nice day";
+    document.getElementById("df").innerHTML = text;
+    const person1 = {
+        fullName:function(){
+            return this.firstName+" "+this.lastName;
+        }
+    }
+    const person2 ={
+        firstName:"John",
+        lastName: "Doe",
+    }
+    let x = person1.fullName.call(person2);
+    document.getElementById("neZ").innerHTML = x;
+    const Carz = {
+        model:"x6",
+        brand:"BMW",
+        fuel:"Petrol",
+        engine:"4000cc",
+        door: 5,
+        reg:function(){
+        return "This car "+this.model+" with engine "+this.engine+" owner by "+this.owner;
+        }
+    }
+    Carz.owner = "salim";
+    // let q = Carz.map(Carz).join("");
+    // document.getElementById("pop").innerHTML = q;
+    let v = Carz.reg.call(Carz);
+    document.getElementById("Vz").innerHTML = v;
