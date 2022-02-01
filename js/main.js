cb1841fb16ae0a21822d87173ec73fde391a440b
saidmtanzania <saidmtanzania@gmail.com>
2022-02-01 22:18:13 +0300

    initial commit

--- js/main.js
+++ js/main.js
@@ -8,3 +8,18 @@ let div = document.getElementById("header");
 function meFun(){
     document.getElementById("demo").innerHTML = "This is a simple Hello unit.";
 }
+/**JavaScript Display Possibilities
+JavaScript can "display" data in different ways:
+    Writing into an HTML element, using innerHTML.
+    Writing into the HTML output using document.write().
+    Writing into an alert box, using window.alert().
+    Writing into the browser console, using console.log()
+ */
+
+    document.getElementById("dev").innerHTML = 5+1;
+    //Using document.write() after an HTML document is loaded, will delete all existing HTML:
+    document.write("take a bow");
+    //Using window.alert() You can use an alert box to display data:
+    window.alert("Hello JavaScript");
+    //window.print() to print the whole page
+    window.print();
