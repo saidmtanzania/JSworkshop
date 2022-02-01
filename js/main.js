15e8bb5f55f598cae645997cf60c59b9703b34ca
saidmtanzania <saidmtanzania@gmail.com>
2022-02-01 22:03:08 +0300

    initial changes

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-document.getElementById("app").innerHTML = "Hello JavaScript";
+let div = document.getElementById("header");
+    div.innerHTML = "Hello JavaScript";
+    div.style.color = "blue";
+/**JavaScript Functions and Events
+    A JavaScript function is a block of JavaScript code, that can be executed when "called" for.
+    For example, a function can be called when an event occurs, like when the user clicks a button.
+ */
+function meFun(){
+    document.getElementById("demo").innerHTML = "This is a simple Hello unit.";
+}
